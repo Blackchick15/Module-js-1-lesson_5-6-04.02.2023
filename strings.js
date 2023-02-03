cfd4a3fff952c124c870eb47c193d05457f02c77
Oksana Petryshyn <opetryshyn2@gmail.com>
2023-02-03 07:22:25 +0200

    для роботи на уроці

---
+++ strings.js
@@ -0,0 +1,48 @@
+//Довжина стороки. Властивість length
+
+const message = 'У цій строці 25 символів.';
+console.log();
+
+//Конкатинація строк
+const furstName = 'Chelsy';
+const lastName = 'Esmerald';
+
+
+/*Напиши скрипт який виводить строку у форматі:
+* "Гість х у поселяється в z номер q"
+* підстав замість x y z q значення змінних.
+*/
+
+const room = 716;
+const type = 'VIP';
+console.log();
+
+
+/* 
+* Шаблонні стороки
+* Створюєємо строку поселення в готель
+*/
+const quantity = 5;
+const ordermsg = `Ви замовили холодильників`;
+console.log();
+
+/*
+Нормалізація з методом toLowerCase()
+*/
+// Що шукаєм?
+
+
+// Метод slice()
+
+
+// Пошук в строці з методом includes()
+
+const blackListedWord1 = 'спам';
+const blackListedWord2 = 'розпродаж';
+
+const string1 = "Привіт, я продавець Абдул, це не спам, пропоную тобі мільйон!";
+const string2 = "Найбільший РОЗПРОДАЖ в цю неділю, не пропустіть!";
+const string3 = "Рекламна компанія #fatLivesmatter";
+
+
+
