fc8f61c251ed5fd770d559c5f3a9765d5e9e801f
Blackchick15 <109077737+Blackchick15@users.noreply.github.com>
2023-02-18 16:44:50 +0200

    commit

--- practice.js
+++ practice.js
@@ -11,14 +11,14 @@ const x2 = 30;
 const number = 50;
 
 
-console.log(`Число ${number} попадає у відрізок до ${x1}`,number < x1 );
+// console.log(`Число ${number} попадає у відрізок до ${x1}`,number < x1 );
 
-console.log(`Число ${number} попадає у відрізок після ${x2}`,number > x2 );
+// console.log(`Число ${number} попадає у відрізок після ${x2}`,number > x2 );
 
-const res1 = number > x1 && number < x2;
-console.log(`Число ${number} попадає у відрізок до ${x1} до ${x2}`, );
-
-console.log(`Число ${number} попадає у відрізок до ${x1} або після ${x2}`, );
+// const res1 = number > x1 && number < x2;
+// console.log(`Число ${number} попадає у відрізок до ${x1} до ${x2}`,res1 );
+// const res2 = number < x1 ||number > x2;
+// console.log(`Число ${number} попадає у відрізок до ${x1} або після ${x2}`,res2 );
 
 
 // ......................
@@ -35,9 +35,9 @@ _ онлайн
 const isOnline = true;
 const isFriend = true;
 const isDnd = false;
-
+const canOpenChat = isOnline && isFriend;
 // const canOpenChat = ;
-// console.log('Можна відкрити чат?', canOpenChat);
+console.log('Можна відкрити чат?', canOpenChat);
 // ..........................
 
 /*
